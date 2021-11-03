05a56abcee15244370e55722d2297fe300d0dca3
Zachary Russell <zach@protechig.com>
2021-11-03 16:00:15 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloFirestore />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloFirestore from './components/HelloFirestore.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloFirestore
   }
 }
 </script>
---
+++ src/components/HelloFirestore.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>Hello Firestore!</h1>    
+    <h2>My Name is {{name}}</h2>
+    The number of the day is: {{ number }}
+
+    <button v-on:click="number++">Increase me!</button>
+    <ul>
+      <li v-for="piece in compositions" :key="piece.Author">{{ piece.Title}}</li>
+    </ul>
+   <h3>The Counter's Length is {{ counter.length }} </h3>
+   <button v-on:click="addDoc">Add item to counter</button>
+  </div>
+</template>
+
+<script>
+import { db } from '../db'
+export default {
+  name: 'HelloFirestore',
+  data: () => {
+    return {
+      name: 'Nick',
+      number: 42,
+      compositions: [],
+      counter: []
+    }
+  },
+methods: {
+  addDoc: function() {
+    db.collection('Counter').add({
+time: new Date()})
+          }
+         },
+  firestore: {
+    compositions: db.collection('compositions').where("Artist", "==", "Bach"),
+    counter: db.collection('Counter')
+             } 
+}
+</script>
+
+
+
+
